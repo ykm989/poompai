1909093e00728ccf20a376817a7d8c7da0a49aa4
김경호 <ykm989@naver.com>
2025-04-06 14:56:00 +0900

    Initial Commit

---
+++ poompai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poompai
+//
+//  Created by 김경호 on 4/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ poompaiTests/poompaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  poompaiTests.swift
+//  poompaiTests
+//
+//  Created by 김경호 on 4/6/25.
+//
+
+import Testing
+@testable import poompai
+
+struct poompaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
